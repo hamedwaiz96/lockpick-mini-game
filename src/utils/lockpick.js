2830208960ddc3cfc2a65cb3ebf19a47e616145a
Hamed Waiz <hamedw96@gmail.com>
2020-07-30 20:09:19 -0700

    add check for lockpick unlock

--- src/utils/lockpick.js
+++ src/utils/lockpick.js
@@ -14,18 +14,86 @@ const DIFFICULTY_X_LENGTH_MAP = {
 }
 
 class Lockpick {
-	constructor() {
+	constructor(difficulty="Novice") {
 		const self = this;
 		// [x, y]
 		const startPos = startPosition;
 		const endPos1 = [startPosition[1], startPosition[0]];
 		const endPos2 = [-startPosition[1], startPosition[0]];
 		this.currentPos = startPosition;
-		this.lockpicks = 100;
-		this.difficulty = "Novice";
+		this.health = Math.floor(100 / (Object.keys(DIFFICULTY_X_LENGTH_MAP).indexOf(difficulty)+1));
+		this.difficulty = difficulty;
 		this.sectorXLength = DIFFICULTY_X_LENGTH_MAP[this.difficulty];
 		this.winningSectorXRange = this.chooseWinningSectorXRange();
 		this.determiningSectorXRange = [(this.winningSectorXRange[0] - (Math.floor(this.sectorXLength/2))), (this.winningSectorXRange[0] + (Math.floor(this.sectorXLength/2)))];
+		this.holdUp = false;
+		this.currentUnlockStatus = 0;
+	}
+
+	turnRight() {
+		const yChange = (this.currentPos[1] < 100) ? 1 : -1
+		if (this.checkValidMove("right")) { this.currentPos = [this.currentPos[0]+1, this.currentPos[1]+yChange]}
+	}
+
+	turnLeft() {
+		const yChange = (this.currentPos[1] >= 100) ? 1 : -1
+		if (this.checkValidMove("left")) { this.currentPos = [this.currentPos[0]-1, this.currentPos[1]+yChange]}
+	}
+
+	tryUnlock() {
+		this.holdUp = true;
+		if (this.currentUnlockStatus < this.findUnlockStatus()) {
+			this.currentUnlockStatus += 1
+		} else if (this.currentUnlockStatus === this.findUnlockStatus()) {
+			if (this.checkIfWon()) {
+				console.log("Winner!");
+				alert("winner!");
+			} else {
+				this.health -= 1;
+				if (this.checkIfLost()) {
+					console.log("Loser!");
+					alert("loser!")
+				}
+			}
+		}
+	}
+
+	letGo() {
+		this.holdUp = false;
+		if (this.currentUnlockStatus > 0) {
+			this.currentUnlockStatus -= 1;
+		}
+	}
+
+	checkInDeterminingSector() {
+		return (this.currentPos[0] >= this.determiningSectorXRange[0]) && (this.currentPos[0] <= this.determiningSectorXRange[0])
+	}
+
+	checkInWinningSector() {
+		return (this.currentPos[0] >= this.winningSectorXRange[0]) && (this.currentPos[0] <= this.winningSectorXRange[0])
+	}
+
+	findUnlockStatus() {
+		if (this.checkInWinningSector()) {
+			return 100
+		}
+		else if (this.checkInDeterminingSector()) {
+			const firstHalf = [this.determiningSectorXRange[0], this.winningSectorXRange[0]];
+			const secondHalf = [this.winningSectorXRange[1], this.determiningSectorXRange[1]];
+			if (this.currentPos[0] < firstHalf[1]) {
+				return (this.currentPos[0]-firstHalf[0])/(firstHalf[1]-firstHalf[0])
+			} else {
+				return (secondHalf[1]-this.currentPos[0])/(secondHalf[1]-secondHalf[0])
+			}
+		} else {
+			return 0
+		}
+	}
+
+	checkValidMove(dir) {
+		const xChange = (dir === "left") ? -1 : 1
+		if (((this.currentPos[0] + xChange) > this.startPos[1]) || ((this.currentPos[0] + xChange) < -this.startPos[1])) {return false};
+		return true;
 	}
 
 	chooseWinningSectorXRange() {
@@ -34,6 +102,14 @@ class Lockpick {
 		const startXSector = LockPick.randomInteger(-sectorDifference, sectorDifference);
 		const dir = LockPick.randomInteger(0, 1);
 		return (!!dir ? [startXSector - this.sectorXLength, startXSector] : [startXSector, startXSector + this.sectorXLength]);
+	}
+
+	checkIfWon() {
+		return this.currentUnlockStatus === 100;
+	}
+
+	checkIfLost() {
+		return this.health === 0;
 	}
 
 	static randomInteger(min, max) {
